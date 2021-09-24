298a4f9d3cda036402ec45ed1b3ae6d823e648f8
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2021-09-24 01:23:51 -0600

    parte2

--- lab6.py
+++ lab6.py
@@ -39,3 +39,65 @@ b.update(b'hello, world3!')
 print('binario:', bin(int( b.hexdigest(), 16))[2:])
 print('hexadecimal:', b.hexdigest())
 print('base64:', codecs.encode(b.digest(), 'base64'))
+
+##############parte2##############
+
+def sha256sum(filename):
+    key = "Guido"
+    h  = sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            palabra = mv[:n]
+        h.update(palabra)
+    return h.hexdigest()
+
+def sha512sum(filename):
+    key = "Guido"
+    h  = sha512()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            palabra = mv[:n]
+        h.update(palabra)
+    return h.hexdigest()
+
+def blake2bsum(filename):
+    key = "hola"
+    h  = blake2b(key=key.encode('utf-8'),digest_size=64)
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            palabra = mv[:n]
+        h.update(palabra)
+    return h.hexdigest()
+
+def verificacion(filename,hashh,key):
+    h  = blake2b(key=key.encode('utf-8'),digest_size=64)
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            palabra = mv[:n]
+        h.update(palabra)
+        h.hexdigest()
+        if(h.hexdigest() == hashh):
+            return "Verificado, el hash corresponde al archivo"
+        else:
+            return "El hash no corresponde al archivo"
+
+print("\nSHA256")
+print("Archivo 1:",sha256sum("ejemplo.txt"))
+print("Archivo 2:",sha256sum("ejemplo2.txt"))
+print("\nSHA512")
+print("Archivo 1:",sha512sum("ejemplo.txt"))
+print("Archivo 2:",sha512sum("ejemplo2.txt"))
+print("\nBLAKE2B")
+print("Archivo 1:",blake2bsum("ejemplo.txt"))
+print("Archivo 2:",blake2bsum("ejemplo2.txt"))
+print("")
+print("Prueba con el hash1 en el archivo1:",verificacion("ejemplo.txt",blake2bsum("ejemplo.txt"),"hola"))
+print("Prueba con el hash2 en el archivo1:",verificacion("ejemplo.txt",blake2bsum("ejemplo2.txt"),"hola"))
